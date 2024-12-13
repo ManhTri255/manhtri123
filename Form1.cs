dee9a21ff95ffbb0cac07ea874926b12f34b33b5
Vũ Mạnh Trí <vumanhtri255@gmail.com>
2024-12-13 21:53:08 +0700

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,37 +1,142 @@
-﻿using System;
+﻿//VuManhTri_2280603381
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Lab03
+namespace Lab03_02
 {
-    public partial class Form1 : Form
+    public partial class frmSoanVanBan : Form
     {
-        public Form1()
+        public frmSoanVanBan()
         {
             InitializeComponent();
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void loadFont()
         {
-            var date = DateTime.Now.ToString("dd/MM/yy");
-            var time = DateTime.Now.ToString("hh:mm:ss tt");
-            this.toolStripStatusLabel1.Text = string.Format($"Hom Nay La Ngay: {date} - Bay Gio La: {time}");
+            foreach (FontFamily fontFamily in new InstalledFontCollection().Families)
+            {
+                cmbFont.Items.Add(fontFamily.Name);
+            }
+            cmbFont.SelectedItem = "Tahoma";
         }
 
-        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        private void loadSize()
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
+            int[] sizeValues = new int[] { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+            cmbSize.ComboBox.DataSource = sizeValues;
+            cmbSize.SelectedItem = 14;
+        }
 
-            openFileDialog.Filter = "Video File|*.mp4;*.avi;*.mkv;*.wmv";
-            if (openFileDialog.ShowDialog() == DialogResult.OK )
+        private void frmSoanVanBan_Load(object sender, EventArgs e)
+        {
+            loadFont();
+            loadSize();
+        }
+
+        private void ToolStripMenuItemDinhDang_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDlg = new FontDialog();
+            fontDlg.ShowColor = true;
+            fontDlg.ShowApply = true;
+            fontDlg.ShowEffects = true;
+            fontDlg.ShowHelp = true;
+            if (fontDlg.ShowDialog() != DialogResult.Cancel)
             {
-                axWindowsMediaPlayer1.URL = openFileDialog.FileName;
+                rtbVanBan.ForeColor = fontDlg.Color;
+                rtbVanBan.Font = fontDlg.Font;
+            }
+        }
+
+        private void ToolStripMenuItemMoFile_Click(object sender, EventArgs e)
+        {
+            rtbVanBan.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|RichText files (*.rtf)|*.rtf";
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFileName = openFileDialog.FileName;
+                try
+                {
+                    if (Path.GetExtension(selectedFileName).Equals(".txt", StringComparison.OrdinalIgnoreCase))
+                    {
+                        rtbVanBan.LoadFile(selectedFileName, RichTextBoxStreamType.PlainText);
+                    }
+                    else
+                    {
+                        rtbVanBan.LoadFile(selectedFileName, RichTextBoxStreamType.RichText);
+                    }
+                    MessageBox.Show("Tap Tin Da Duoc Mo Thanh Cong!!!", "Thong Bao", MessageBoxButtons.OK);
+                }
+
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Da Xay Ra Loi Trong Qua Trinh Mo Tap Tin: " + ex.Message, "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btn_Bold_Click(object sender, EventArgs e)
+        {
+            if (rtbVanBan.SelectionFont != null)
+            {
+                FontStyle style = rtbVanBan.SelectionFont.Style;
+                if (rtbVanBan.SelectionFont.Bold)
+                {
+                    style &= ~FontStyle.Bold;
+                }
+                else
+                {
+                    style |= FontStyle.Bold;
+                }
+                rtbVanBan.SelectionFont = new Font(rtbVanBan.SelectionFont, style);
+            }
+        }
+
+        private void btn_Italic_Click(object sender, EventArgs e)
+        {
+            if (rtbVanBan.SelectionFont != null)
+            {
+                FontStyle style = rtbVanBan.SelectionFont.Style;
+                if (rtbVanBan.SelectionFont.Italic)
+                {
+                    style &= ~FontStyle.Italic;
+                }
+                else
+                {
+                    style |= FontStyle.Italic;
+                }
+                rtbVanBan.SelectionFont = new Font(rtbVanBan.SelectionFont, style);
+            }
+        }
+
+        private void btn_Under_Click(object sender, EventArgs e)
+        {
+            if (rtbVanBan.SelectionFont != null)
+            {
+                FontStyle style = rtbVanBan.SelectionFont.Style;
+                if (rtbVanBan.SelectionFont.Underline)
+                {
+                    style &= ~FontStyle.Underline;
+                }
+                else
+                {
+                    style |= FontStyle.Underline;
+                }
+                rtbVanBan.SelectionFont = new Font(rtbVanBan.SelectionFont, style);
             }
         }
     }
